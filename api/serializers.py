f2e823af20d8d40452a3c87320cfa79cf7321b33
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-02-13 15:24:08 +0900

    testz

--- api/serializers.py
+++ api/serializers.py
@@ -3,9 +3,6 @@ from .models import Company, Office, Service
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-
     class Meta:
         model = User
         fields = ('id', 'username', 'password', "created_at", "updated_at")
