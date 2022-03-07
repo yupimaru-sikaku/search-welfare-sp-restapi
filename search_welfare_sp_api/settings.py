4d38e95dadcb94cc0d2511143d9c214c4ed2edd2
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-03-07 11:48:32 +0900

    TEST

--- search_welfare_sp_api/settings.py
+++ search_welfare_sp_api/settings.py
@@ -68,8 +68,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
-
 ROOT_URLCONF = 'search_welfare_sp_api.urls'
 
 TEMPLATES = [
